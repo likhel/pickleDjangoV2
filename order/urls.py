e93c229ec752c11b9932f7ef2d20a33d1e626bba
likhel <134918357+likhel@users.noreply.github.com>
2025-07-22 22:49:16 +0545

    Update urls.py

--- order/urls.py
+++ order/urls.py
@@ -4,16 +4,14 @@ from rest_framework_nested.routers import NestedSimpleRouter
 
 from .views import OrderViewSet, OrderItemViewSet
 
-# Create a router for the OrderViewSet
 router = DefaultRouter()
 router.register(r'orders', OrderViewSet, basename='order')
 
-# Create a nested router for OrderItemViewSet
-# This will create URLs like /orders/{order_pk}/items/
+
 orders_router = NestedSimpleRouter(router, r'orders', lookup='order')
 orders_router.register(r'items', OrderItemViewSet, basename='order-item')
 
 urlpatterns = [
-    path('', include(router.urls)), # Include the main order router URLs
-    path('', include(orders_router.urls)), # Include the nested order item router URLs
+    path('', include(router.urls)),
+    path('', include(orders_router.urls)),
 ]
